50b1f2ae1cd11de4268ad2ebf5d390d16e1493bc
SilentZeroOne <a792038524@gmail.com>
2020-04-01 15:43:38 +0800

    观察者模式——事件委托实现

---
+++ ObserverPattern/Observer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    
+
+    class StockObserver 
+    {
+
+        private string name;
+        private Subject sub;
+        public StockObserver(string name, Subject sub)
+        {
+            this.name = name;
+            this.sub = sub;
+        }
+
+        public void CloseStock()
+        {
+            Console.WriteLine("{0} {1} 关闭股票市场，继续工作！", sub.SubjectState,name);
+        }
+
+    }
+    class NbaObserver
+    {
+
+        private string name;
+        private Subject sub;
+        public NbaObserver(string name, Subject sub)
+        {
+            this.name = name;
+            this.sub = sub;
+        }
+
+        public void CloseNba()
+        {
+            Console.WriteLine("{0} {1} 关闭NBA，继续工作！",sub.SubjectState,name);
+        }
+
+    }
+}
---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boss huhansan = new Boss();
+            Secretary xiaomi = new Secretary();
+
+            StockObserver tongshi1 = new StockObserver("必胜客", huhansan);
+            NbaObserver tongshi2 = new NbaObserver("臭弟弟", xiaomi);
+
+            huhansan.Update += new EventHandler(tongshi1.CloseStock);
+            xiaomi.Update += new EventHandler(tongshi2.CloseNba);
+
+            huhansan.SubjectState = "我胡汉三有回来了！";
+            xiaomi.SubjectState = "老板回来了！";
+
+
+            huhansan.Notify();
+            xiaomi.Notify();
+
+            Console.Read();
+        }
+    }
+}
