0e406dd06873c3cb40f1c6e50b267f5a70c84b1a
SilentZeroOne <a792038524@gmail.com>
2020-04-02 14:38:07 +0800

    单例模式

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            if (s1 == s2)
+            {
+                Console.WriteLine("相同实例");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private Singleton(){}
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+    class Singleton2//禁止多线程访问，加入锁,双层锁定需要在锁外层在加一个if(instance==null)
+    {
+        private static Singleton2 instance;
+        private static readonly object syncRoot = new object();
+        private Singleton2() { }
+        public static Singleton2 GetSingleton()
+        {
+            lock (syncRoot)
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton2();
+                }
+            }
+            return instance;
+        }
+    }
+}
