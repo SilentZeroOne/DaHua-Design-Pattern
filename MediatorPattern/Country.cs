0f4c6a842d2a4a76013da7d008f0438b7f644259
SilentZeroOne <a792038524@gmail.com>
2020-04-04 15:06:07 +0800

    中介者模式

---
+++ MediatorPattern/Country.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    abstract  class Country
+    {
+        protected UnitedNations mediator;
+        public Country(UnitedNations mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+    class USA : Country
+    {
+        public USA(UnitedNations mediator) : base(mediator) { }
+        public void Declare(string message)
+        {
+            mediator.Declare(message, this);
+        }
+        public void GetMessage(string message)
+        {
+            Console.WriteLine("美国获得对方信息：" + message);
+        }
+    }
+    class Iraq : Country
+    {
+        public Iraq(UnitedNations mediator) : base(mediator) { }
+        public void Declare(string message)
+        {
+            mediator.Declare(message, this);
+        }
+        public void GetMessage(string message)
+        {
+            Console.WriteLine("伊拉克获得对方信息：" + message);
+        }
+    }
+}
---
+++ MediatorPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Council united = new Council();
+            USA usa = new USA(united);
+            Iraq iraq = new Iraq(united);
+
+            united.Country1 = usa;
+            united.Country2 = iraq;
+
+            usa.Declare("不准研制核武器！");
+            iraq.Declare("你打我呀！");
+
+            Console.Read();
+        }
+    }
+}
